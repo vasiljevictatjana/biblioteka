a1d37cc264bcaaf5399b579939a4672b1e3f23df
vasiljevictatjana813 <vasiljevictatjana@yahoo.com>
2015-03-12 15:27:03 -0700

    Dodate toString, equals and hashCode u klase Autor i Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -12,6 +12,7 @@ public class Knjiga {
 	private LinkedList<Autor> autori = new LinkedList<Autor>();
 	
 	
+	
 	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
@@ -44,6 +45,31 @@ public class Knjiga {
 		this.izdanje = izdanje;
 	}
 	
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", izdavac=" + izdavac + ", ISBN="
+				+ ISBN + ", izdanje=" + izdanje + ", autori=" + autori + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (ISBN ^ (ISBN >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (ISBN != other.ISBN)
+			return false;
+		return true;
+	}
 	
 
 }
